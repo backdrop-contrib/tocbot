d946993da268d6f2cd2b3538d2b413605c78dad5
Nicholas Stees <nstees@gmail.com>
2019-06-26 17:22:43 -0400

    making coding standards corrections

--- js/tocbot-init.js
+++ js/tocbot-init.js
@@ -1,25 +1,25 @@
 (function($) {
-  "use strict";
+  'use strict';
 
   Drupal.behaviors.tocbot = {
     attach: function attachTocBot(context, settings) {
       if (
         $(settings.tocbot.options.tocSelector).length &&
-        $(settings.tocbot.options.contentSelector + " :header").not(
+        $(settings.tocbot.options.contentSelector + ' :header').not(
           settings.tocbot.options.ignoreSelector
         ).length >= parseInt(settings.tocbot.minActivate)
       ) {
         // Activate
         if (settings.tocbot.extrabodyclass.length > 0) {
-          $("body").addClass(settings.tocbot.extrabodyclass);
+          $('body').addClass(settings.tocbot.extrabodyclass);
         }
 
         tocbot.init(settings.tocbot.options);
         if (settings.tocbot.tocTitle.length > 0) {
-          $(".js-toc").prepend(
-            "<strong class='toc-title'>" +
+          $('.js-toc').prepend(
+            '<strong class=\'toc-title\'>' +
               settings.tocbot.tocTitle +
-              "</strong>"
+              '</strong>'
           );
         }
       }
